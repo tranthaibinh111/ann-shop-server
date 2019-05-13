3fd4a9ab3a62efcac7babbf93f651e71ccfd90fc
Binh Tran <tranthaibinh111@yahoo.com>
2019-05-13 17:22:12 +0700

    First init Project: - Docker     +) Graphviz     +) Django: RESTFull

---
+++ compose/django/start.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function manage_app () {
+    python manage.py migrate
+    python manage.py collectstatic --no-input
+}
+
+function start_development() {
+    # use django runserver as development server here.
+    manage_app
+    python manage.py runserver 0.0.0.0:8000
+}
+
+function start_production() {
+    # use gunicorn for production server here
+    manage_app
+    gunicorn config.wsgi -w 4 -b 0.0.0.0:8000 --chdir=/app --log-file -
+}
+
+if [ "$DEBUG" = "True" ]
+then
+    # use development server
+    start_development
+else
+    # use production server
+    start_production
+fi
